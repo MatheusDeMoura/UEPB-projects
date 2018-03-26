0e9caf73a79f29d9834e721b1e1b1dd9ecbaef68
kobby <kobby@192.168.25.239>
2018-03-26 16:05:39 -0300

    Atividade Lab02, referente à cadeira de Laboratório de Programação.

--- Documents/NetBeansProjects/Lab02/src/Lab02/PedraPapelTesoura.java
+++ Documents/NetBeansProjects/Lab02/src/Lab02/PedraPapelTesoura.java
@@ -2,25 +2,52 @@
 package Lab02;
 
 /**
- *
- * 
- */
+ *GRUPO:
+ *  - Felipe da Silva Gouveia
+ *  - 
+ *  - 
+ **/
 import java.util.Scanner;
-import java.lang.Math;
 
 public class PedraPapelTesoura {
+    static Scanner s = new Scanner(System.in);
     public static void main (String []args){
-        final int NUMERO_SIMBOLOS = 3, NUMERO_DE_JOGADORES = 2, PEDRA = 1, PAPEL = 2, TESOURA = 3;
+        final int NUMERO_SIMBOLOS = 3;
         int jogador1, jogador2, vencedor;
-        Scanner s = new Scanner(System.in);
-        System.out.printf("Digite o número correspondente a opção de jogador 1.\nPedra (1), Papel (2), Tesoura (3)? ");
-        jogador1 = s.nextInt();
-        System.out.printf("Digite o número correspondente a opção de jogador 2.\nPedra (1), Papel (2), Tesoura (3)? ");
-        jogador2 = s.nextInt();
-        vencedor = compararSimbolos( jogador1, jogador2, NUMERO_SIMBOLOS);
+        jogador1 = lerOpcaoJogador1();
+        jogador2 = lerOpcaoJogador2();
+        System.out.println(opcao1(jogador1) + " - " + opcao2(jogador2));
 
     }
     private static int compararSimbolos(int jogador1, int jogador2, int NUMERO_SIMBOLOS){
         return Math.abs(NUMERO_SIMBOLOS + jogador1 - jogador2);
+    }
+
+    private static int lerOpcaoJogador1() {
+        System.out.printf("Digite o número correspondente a opção de jogador 1.\nPedra (1), Papel (2), Tesoura (3)? ");
+        return s.nextInt();
+    }
+
+    private static int lerOpcaoJogador2() {
+        System.out.printf("Digite o número correspondente a opção de jogador 2.\nPedra (1), Papel (2), Tesoura (3)? ");
+        return s.nextInt();
+    }
+
+    private static String opcao1(int jogador1) {
+        switch(jogador1){
+            case 1: return "Pedra";
+            case 2: return "Papel";
+            case 3: return "Tesoura";
+        }
+        return "0";
+    }
+
+    private static String opcao2(int jogador2) {
+        switch(jogador2){
+            case 1: return "Pedra";
+            case 2: return "Papel";
+            case 3: return "Tesoura";
+        }
+        return "0";
     }
 }
