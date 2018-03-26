293ba748a8330d9255e64eacda2bc7ca1399214d
kobby <kobby@192.168.25.239>
2018-03-26 16:49:29 -0300

    Atividade Lab02, referente à cadeira de Laboratório de Programação.

--- Documents/NetBeansProjects/Lab02/src/Lab02/PedraPapelTesoura.java
+++ Documents/NetBeansProjects/Lab02/src/Lab02/PedraPapelTesoura.java
@@ -14,11 +14,12 @@ public class PedraPapelTesoura {
     public static void main (String []args){
         final int NUMERO_SIMBOLOS = 3;
         int jogador1, jogador2;
-        jogador1 = lerOpcaoJogador1();
-        jogador2 = lerOpcaoJogador2();
+        jogador1 = lerOpcaoJogador1(NUMERO_SIMBOLOS);
+        jogador2 = lerOpcaoJogador2(NUMERO_SIMBOLOS);
         System.out.println(opcao1(jogador1) + " - " + opcao2(jogador2));
         declararVencedor(jogador1, jogador2, NUMERO_SIMBOLOS);
     }
+    
     private static void declararVencedor(int jogador1, int jogador2, int NUMERO_SIMBOLOS){
         double vencedor;
         vencedor = Math.abs(jogador1 - jogador2);
@@ -29,16 +30,26 @@ public class PedraPapelTesoura {
         }else System.out.println("Jogador 2 Ganhou!");
     }
 
-    private static int lerOpcaoJogador1() {
+    private static int lerOpcaoJogador1(int NUMERO_SIMBOLOS) {
         System.out.printf("Digite o número correspondente a opção de jogador 1.\nPedra (1), Papel (2), Tesoura (3)? ");
-        return s.nextInt();
+        int jogador1 = s.nextInt();
+        while(jogador1 < 1 || jogador1 > 3){
+            System.out.println("Opção invalida, por favor digite uma opção entre 1 e 3.");
+            jogador1 = s.nextInt();
+        }
+        return jogador1;
     }
 
-    private static int lerOpcaoJogador2() {
-        System.out.printf("Digite o número correspondente a opção de jogador 2.\nPedra (1), Papel (2), Tesoura (3)? ");
-        return s.nextInt();
+    private static int lerOpcaoJogador2(int NUMERO_SIMBOLOS) {
+        System.out.printf("Digite o número correspondente a opção de jogador 1.\nPedra (1), Papel (2), Tesoura (3)? ");
+        int jogador2 = s.nextInt();
+        while(jogador2 < 1 || jogador2 > 3){
+            System.out.println("Opção invalida, por favor digite uma opção entre 1 e 3.");
+            jogador2 = s.nextInt();
+        }
+        return jogador2;
     }
-
+    
     private static String opcao1(int jogador1) {
         switch(jogador1){
             case 1: return "PEDRA";
