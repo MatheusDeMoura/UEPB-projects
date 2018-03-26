67c536175022a771747364f05584ad12fdd25889
kobby <kobby@192.168.25.239>
2018-03-26 16:30:08 -0300

    Atividade Lab02, referente à cadeira de Laboratório de Programação.

--- Documents/NetBeansProjects/Lab02/src/Lab02/PedraPapelTesoura.java
+++ Documents/NetBeansProjects/Lab02/src/Lab02/PedraPapelTesoura.java
@@ -13,14 +13,20 @@ public class PedraPapelTesoura {
     static Scanner s = new Scanner(System.in);
     public static void main (String []args){
         final int NUMERO_SIMBOLOS = 3;
-        int jogador1, jogador2, vencedor;
+        int jogador1, jogador2;
         jogador1 = lerOpcaoJogador1();
         jogador2 = lerOpcaoJogador2();
         System.out.println(opcao1(jogador1) + " - " + opcao2(jogador2));
-
+        declararVencedor(jogador1, jogador2, NUMERO_SIMBOLOS);
     }
-    private static int compararSimbolos(int jogador1, int jogador2, int NUMERO_SIMBOLOS){
-        return Math.abs(NUMERO_SIMBOLOS + jogador1 - jogador2);
+    private static void declararVencedor(int jogador1, int jogador2, int NUMERO_SIMBOLOS){
+        double vencedor;
+        vencedor = Math.abs(jogador1 - jogador2);
+        if(vencedor == 0){
+            System.out.println("Enpate!");
+        }else if(vencedor <= NUMERO_SIMBOLOS/2){
+            System.out.println("Jogador 1 Ganhou!");
+        }else System.out.println("Jogador 2 Ganhou!");
     }
 
     private static int lerOpcaoJogador1() {
@@ -35,18 +41,18 @@ public class PedraPapelTesoura {
 
     private static String opcao1(int jogador1) {
         switch(jogador1){
-            case 1: return "Pedra";
-            case 2: return "Papel";
-            case 3: return "Tesoura";
+            case 1: return "PEDRA";
+            case 2: return "PAPEL";
+            case 3: return "TESOURA";
         }
         return "0";
     }
 
     private static String opcao2(int jogador2) {
         switch(jogador2){
-            case 1: return "Pedra";
-            case 2: return "Papel";
-            case 3: return "Tesoura";
+            case 1: return "PEDRA";
+            case 2: return "PAPEL";
+            case 3: return "TESOURA";
         }
         return "0";
     }
